54a3a33a58f7a0a2d8688bbf1a19b8fc41f04946
Bob Cui <xiaojia1100@163.com>
2018-11-07 20:38:20 +0800

    fix(dma): refine struct

--- demos/demo-maven-archetype/src/main/resources/archetype-resources/src/main/java/__artifactId__/api/controller/DemoController.java
+++ demos/demo-maven-archetype/src/main/resources/archetype-resources/src/main/java/__artifactId__/api/controller/DemoController.java
@@ -1,4 +1,4 @@
-package ${package}.${artifactId}.controller;
+package ${package}.${artifactId}.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
